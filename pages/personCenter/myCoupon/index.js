c5164ec17d95ef4b7a5d62830e647044c64fd472
MTmatao <1414294651@qq.com>
2018-11-18 12:44:47 +0800

    优惠券

--- pages/personCenter/myCoupon/index.js
+++ pages/personCenter/myCoupon/index.js
@@ -20,10 +20,9 @@ Page({
         { text: '已使用', value: '2' },
         { text: '已过期', value: '0' },
       ],
-      // 优惠券列表
-      couponList: [],
-
     },
+    // 优惠券列表
+    couponList: [],
     
     // 滚动距离
     scrollWidth: 200 * app.GO.rpxValue ,
@@ -53,6 +52,9 @@ Page({
         })
       }
     })
+
+    // 获取数据
+    this.getData();
   },
 
   // 切换选项
@@ -64,6 +66,120 @@ Page({
       scrollIndex: e.target.dataset.index
     })
 
+    let pageData = this.data.pageData;
+    pageData.current = 1;
+
+    this.setData({
+      pageData: pageData,
+      couponList: []
+    })
+
+    this.getData();
   }
+  ,
+
+  /**
+  * 数据加载
+  *（上拉加载更多）
+  */
+  getData() {
+
+    // 没有更多数据了
+    if (this.data.pageData.finished) {
+      wx.showToast({ title: '没有更多了！', icon: 'none' });
+      return;
+    }
+
+    wx.showLoading({ title: '加载中' })
+
+    // 课程接口参数
+    let url = app.GO.api + 'customer/coupon/getCouponList';
+    let param = {
+      pageNo: this.data.pageData.current,
+      pageSize: this.data.pageData.pageSize,
+      ciCode: app.GO.recommend_customer_id, //获取用户code
+      couponStatus: this.data.couponData.couponType[this.data.scrollIndex].value,
+      couponForm: ''
+    };
+
+    app.appRequest('post', url, param, {}, (res) => {
+      wx.hideLoading()
+      console.log(res)
+      if (res.code == 200) {
+
+        let data = res.content.list;
+        let couponList = this.data.couponList;
+        let pageData = this.data.pageData;
+
+        pageData.total = res.content.count;
+        pageData.current++;
+
+        for (let item of data) {
+
+          let color = '', stateImg = '';
+
+          // couponStatus (string, optional): 优惠券状态 0-未使用 1-已使用 2-已过期 3-使用中 
+          switch (item.couponStatus) {
+            case 0: 
+              color = '#0a8'; 
+              stateImg = '';
+              break;
+            case 1: 
+              color = '#f09105'; 
+              stateImg = '../../../images/image/coupon-icon_01.png'; 
+              break;
+            case 2: 
+              color = '#f8cca4'; 
+              stateImg = '../../../images/image/coupon-icon_02.png'; 
+              break;
+            case 3: 
+              color = '#ccc'; 
+              stateImg = '../../../images/image/coupon-icon_01.png'; 
+              break;
+          }
+
+          couponList.push({
+            price: item.couponInfo.couponValuePrice,
+            content: item.couponInfo.couponTitle,
+            time: this.dateFormat(item.couponInfo.couponStartTime) + '-' + this.dateFormat(item.couponInfo.couponEndTime),
+            stateImg: stateImg,
+            color: color,
+            platform: ''
+          })
+        }
+
+        // 数据全部加载完成
+        if (couponList.length >= pageData.total) {
+
+          pageData.finished = true;
+          wx.showToast({ title: '没有更多了！', icon: 'none' })
+
+        }
+
+        this.setData({
+          couponList: couponList,
+          pageData: pageData
+        })
+        console.log(this.data.couponList)
+
+      } else {
+
+        wx.showToast({ title: res.message })
+
+      }
+    }, (err) => {
+      console.log('请求错误信息：  ' + err.errMsg);
+    });
+
+  },
+  //时间格式化函数，此处仅针对yyyy-MM-dd hh:mm:ss 的格式进行格式化
+  dateFormat(time) {
+    var date = new Date(time);
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1;
+    var day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+    // 拼接
+    return year + "/" + month + "/" + day;
+  },
 
 })
