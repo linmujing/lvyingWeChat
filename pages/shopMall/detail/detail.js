4c03f49f4082351d6f8348aa2da4327c1c337c83
linmujing <543302839@qq.com>
2018-11-19 17:58:07 +0800

    详情

--- pages/shopMall/detail/detail.js
+++ pages/shopMall/detail/detail.js
@@ -1,18 +1,44 @@
 // pages/shopMall/detail/detail.js
+var app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    detailData: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.getDetailData()
+  },
+  // 获取详情
+  getDetailData() {
+    wx.showLoading({ title: '加载中' })
+    var that = this;
+    // 接口参数
+    let url = app.GO.api + 'product/info/getProductInfo';
+    // let param = { productCode: 1, ciCode: app.GO.recommend_customer_id };
+    let param = { productCode: 'P154043959943866', ciCode: app.GO.recommend_customer_id };
+    app.appRequest('post', url, param, {}, (res) => {
+      wx.hideLoading()
+      console.log(res)
+      if (res.code == 200) {
+        that.setData({
+          detailData: res.content
+        })
+      } else {
 
+        wx.showToast({ title: res.message, icon: 'none' })
+
+      }
+    }, (err) => {
+      wx.hideLoading()
+      console.log('请求错误信息：  ' + err.errMsg);
+    });
   },
 
   /**
